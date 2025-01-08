2b5ab26086e27c1259093d225c5175ce5215fa5f
ankit00010 <ankitmish83@gmail.com>
2025-01-08 20:45:12 +0530

    feat:"added const_routes.ts to check the server health in a constants folder"

--- dist/index.js
+++ dist/index.js
@@ -8,12 +8,14 @@ const dotenv_1 = __importDefault(require("dotenv"));
 const cors_1 = __importDefault(require("cors"));
 const chatbot_routes_1 = __importDefault(require("./modules/chatbot/routes/chatbot_routes"));
 const database_1 = require("./config/database");
+const const_routes_1 = require("./constants/const_routes");
 dotenv_1.default.config();
 (0, database_1.initalizeMongo)();
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 const port = process.env.PORT || 8001;
+app.use(const_routes_1.const_routes);
 app.use("/api", chatbot_routes_1.default);
 app.listen(port, () => {
     console.log(`Server is running on ${port}......`);
