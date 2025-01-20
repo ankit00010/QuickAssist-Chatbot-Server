4c97eaaa0b2e546da8cc390218b71a2fa4927de2
ankit00010 <ankitmish83@gmail.com>
2025-01-21 00:41:57 +0530

    feat: Create sendOtpMail function to send OTP via email using Nodemailer

--- src/utils/chatbot_utils.ts
+++ src/utils/chatbot_utils.ts
@@ -1,3 +1,5 @@
+import nodemailer from 'nodemailer';
+import ThrowError from '../middleware/error';
 class ChatBotUtils {
 
 
@@ -10,6 +12,79 @@ class ChatBotUtils {
 
         return `${year}${month}${count}`;
     }
+
+
+
+
+
+    static generateOtp(): number {
+
+
+        const randomNumber: number = Math.floor(Math.random() * 900000) + 100000;
+
+        return randomNumber
+    }
+
+
+
+    static async sendOtpMail(
+        otp: string
+    ): Promise<any> {
+
+        console.log("The otp is :=>", otp);
+
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: process.env.GMAIL,
+                pass: process.env.PASSWORD
+            }
+        })
+
+
+        console.log("The Mail is :=>", process.env.GMAIL);
+        console.log("The Password is :=>", process.env.PASSWORD);
+
+
+        const mailOptions = {
+            from: process.env.GMAIL, // sender address
+            to: "ankitmish83@gmail.com", // recipient address
+            subject: 'Your OTP Code for Verification', // Subject line
+            text: `Hello, 
+          
+          Here is your One-Time Password (OTP) for authentication: 
+          
+          ${otp} 
+          
+          Please use this code to complete your action. This code will expire in 1 minute.
+          
+          If you did not request this, please ignore this email.`,
+
+            html: `
+              <p>Hello,</p>
+              <p>Here is your One-Time Password (OTP) for authentication:</p>
+              <h2 style="color: #4CAF50;">${otp}</h2>
+              <p>Please use this code to complete your action. This code will expire in 1 minute.</p>
+              <p>If you did not request this, please ignore this email.</p>
+            `,
+        };
+        return new Promise<boolean>((resolve, reject) => {
+            transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    console.log('Error sending email:', error);
+                    reject(false)
+                } else {
+                    console.log('Email sent: ', info.response);
+                    resolve(true);
+
+                }
+            })
+        })
+    }
+
+
+
+
 
 }
 
