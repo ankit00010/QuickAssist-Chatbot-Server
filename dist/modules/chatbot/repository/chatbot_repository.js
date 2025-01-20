8b26fb31e6ba6d303c4b194d83926cbe67f1dbf5
ankit00010 <ankitmish83@gmail.com>
2025-01-21 00:40:43 +0530

    feat:"Shifted Admin Related functions to admin Module "

--- dist/modules/chatbot/repository/chatbot_repository.js
+++ dist/modules/chatbot/repository/chatbot_repository.js
@@ -72,43 +72,5 @@ class WhatsappChatbotRepository {
             return findData;
         });
     }
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    //Find Data in a Database by Id
-    static findDataById(id) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const db = yield database_1.client.db("master");
-            const find_data = yield db.collection("faq_info").findOne({
-                faq_id: id,
-            });
-            if (!find_data) {
-                return false;
-            }
-            return true;
-        });
-    }
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    // Update Data function
-    static updateData(id, field) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const db = yield database_1.client.db("master");
-            const updateData = yield db.collection("faq_info").findOneAndUpdate({ faq_id: id }, // Find the document by faq_id
-            { $set: field }, // Use $set to update the provided fields in "field"
-            { returnDocument: 'after' });
-            return updateData.value !== null;
-        });
-    }
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    static deleteFaqData(id) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const db = yield database_1.client.db("master");
-            const deleteData = yield db.collection("faq_info").findOneAndDelete({
-                faq_id: id
-            });
-            if (deleteData) {
-                return true;
-            }
-            return false;
-        });
-    }
 }
 exports.default = WhatsappChatbotRepository;
