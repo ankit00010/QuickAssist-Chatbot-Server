28a9af0e5833c2532577ec8d6a193350f1ff7dbe
ankit00010 <ankitmish83@gmail.com>
2025-01-21 00:41:19 +0530

    feat:"Shifted addData,editData and deleteData routes to admin routes"

--- dist/modules/chatbot/routes/chatbot_routes.js
+++ dist/modules/chatbot/routes/chatbot_routes.js
@@ -8,7 +8,4 @@ const chatbot_controllers_1 = __importDefault(require("../controller/chatbot_con
 const chatbat_routes = (0, express_1.Router)();
 chatbat_routes.get("/whatsapp-webhook", chatbot_controllers_1.default.verifyWebHooks);
 chatbat_routes.post("/whatsapp-webhook", chatbot_controllers_1.default.receiveWhatsappQuery);
-chatbat_routes.post("/add-data", chatbot_controllers_1.default.addData);
-chatbat_routes.put("/edit-data/:id", chatbot_controllers_1.default.editData);
-chatbat_routes.delete("/delete-data/:id", chatbot_controllers_1.default.deleteData);
 exports.default = chatbat_routes;
--- src/modules/chatbot/routes/chatbot_routes.ts
+++ src/modules/chatbot/routes/chatbot_routes.ts
@@ -7,10 +7,6 @@ const chatbat_routes= Router();
 
 chatbat_routes.get("/whatsapp-webhook",WhatsappChatbot.verifyWebHooks);
 chatbat_routes.post("/whatsapp-webhook",WhatsappChatbot.receiveWhatsappQuery);
-chatbat_routes.post("/add-data",WhatsappChatbot.addData);
-chatbat_routes.put("/edit-data/:id",WhatsappChatbot.editData);
-chatbat_routes.delete("/delete-data/:id",WhatsappChatbot.deleteData);
-
 
 
 
