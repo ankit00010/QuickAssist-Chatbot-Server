f2d714efebd99a6bd3257f2246cedb27febab5a5
ankit00010 <ankitmish83@gmail.com>
2025-01-21 00:32:15 +0530

    feat: Set up authentication and admin routes in index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import cors from "cors";
 import chatbat_routes from "./modules/chatbot/routes/chatbot_routes";
 import { initalizeMongo } from "./config/database";
 import { const_routes } from "./constants/const_routes";
+import admin_routes from "./modules/admin/routes/admin_routes";
+import auth_routes from "./modules/auth/routes/auth_routes";
 
 dotenv.config();
 initalizeMongo();
@@ -15,7 +17,8 @@ const port =process.env.PORT || 8001;
 
 app.use(const_routes);
 app.use("/api",chatbat_routes);
-
+app.use("/api/auth",auth_routes);
+app.use("/api/admin",admin_routes);
 
 
 app.listen(port,()=>{
